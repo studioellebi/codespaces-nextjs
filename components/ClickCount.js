bc7da6f496ed593d9c5422b8bbe1cd0cd73f825f
studioellebi <studioellebi01@gmail.com>
2023-05-13 12:25:01 +0000

    add - subt

--- components/ClickCount.js
+++ components/ClickCount.js
@@ -3,9 +3,12 @@ import Button from './Button'
 
 export default function ClickCount() {
   const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
-  return <Button onClick={increment}>Clicks: {count}</Button>
+  const increment = useCallback(() => { setCount((v) => v + 1 )}, [setCount])
+  const decrement = useCallback(() => { setCount((v) => ( v>0 ? v - 1 : v))}, [setCount])
+  // (v>0 ? v - 1 : 0) 
+  return <div>
+      <Button onClick={increment}> Add   </Button> 
+      <b> [ {count} ] </b>
+      <Button onClick={decrement}> Subt  </Button>
+      </div>
 }
